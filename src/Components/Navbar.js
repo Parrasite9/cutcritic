d075c45551e2e14a2d87f657797625d485e4fb2a
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-03-27 11:46:31 -0500

    Navbar now has a conditional operaterator that displays the menu based on screen width. We had this capability before, but what makes this different is that before, the menu links displayed underneath the Logo, which gave an appearance of a line break or something. This was because the Burger Icon, even though it was not being displayed on a Desktop view, it was still there 'per se.' As a rememdy, an isMobile hook was created and based on this hook, the menu is displayed one way or the other. They may look the say, but the logoMenuContainer end div has a different placement from each other. This hook was also added inside of the useEffect.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,6 +14,8 @@ function Navbar() {
   const [showMenu, setShowMenu] = useState(false) 
   // CURRENT LOG IN STATE 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // CURRENT MOBILE STATE 
+  const [isMobile, setIsMobile] = useState(true)
 
   // ====================================
   // CREATE A FUNCTION THAT CHANGES 
@@ -35,15 +37,19 @@ function Navbar() {
   useEffect(() => {
     if (window.innerWidth >= 900) {
       setShowMenu(true)
+      setIsMobile(false)
     } else {
       setShowMenu(false)
+      setIsMobile(true)
     }
 
     function handleResize() {
       if (window.innerWidth >= 900) {
         setShowMenu(true)
+        setIsMobile(false)
       } else {
         setShowMenu(false)
+        setIsMobile(true)
       }
     }
 
@@ -54,46 +60,101 @@ function Navbar() {
 
   return (
     <div className='navbar'>
-      <div className="navbar__Container">
-        <div className="logo__Menu__Container">
-          <h1>Cut Critic</h1>
+      <div className="navbar__Container">       
 
-         {/* Add onClick event listener to MenuIcon to toggle the menu open and closed */}
+      {/* THIS IS FOR MOBILE DISPLAY */}
+      {isMobile && (
+         <>
+            <div className="logo__Menu__Container">
+              <h1>Cut Critic</h1>
 
-          <IconButton className='mobileIcons' alignItems="center" size='large'>
-            {showMenu ? (
-              <CloseIcon fontSize='2em' onClick={toggleMenu} />
-            ) : (
-              <MenuIcon fontSize='2em' onClick={toggleMenu} />
+              {/* Add onClick event listener to MenuIcon to toggle the menu open and closed */}
+
+              <IconButton className='mobileIcons' alignItems="center" size='large'>
+                {showMenu ? (
+                  <CloseIcon fontSize='2em' onClick={toggleMenu} />
+                ) : (
+                  <MenuIcon fontSize='2em' onClick={toggleMenu} />
+                )}
+              </IconButton>
+            </div>
+            
+
+            {/* Use conditional rendering to show the menu only when menuOpen is true */}
+          
+            {showMenu && (
+              <div className="navlinks">
+                {/* IS THE USER CURRENTLY LOGGED IN?  */}
+                {isLoggedIn ? (
+                  <>
+                    <div className="navlink__Item1">Home</div>
+                    <div className="navlink__Item2">Link1</div>
+                    <div className="navlink__Item3">Link2</div>
+                    <div className="navlink__Item4">Link3</div>
+                    <div className="navlink__Item5">Logout</div>
+                  </>
+                ) : (
+                  // IS THE USER CURRENTLY LOGGED OUT? 
+                  <>
+                    <div className="navlink__Item1">Home</div>
+                    <div className="navlink__Item2">Link1</div>
+                    <div className="navlink__Item3">Link2</div>
+                    <div className="navlink__Item4">Link3</div>
+                    <div className="navlink__Item5">Login</div>
+                  </>
+                )}
+              </div>
             )}
-          </IconButton>
-        </div>
 
-       {/* Use conditional rendering to show the menu only when menuOpen is true */}
-       
-       {showMenu && (
-        <div className="navlinks">
-          {/* IS THE USER CURRENTLY LOGGED IN?  */}
-          {isLoggedIn ? (
-            <>
-              <div className="navlink__Item1">Home</div>
-              <div className="navlink__Item2">Link1</div>
-              <div className="navlink__Item3">Link2</div>
-              <div className="navlink__Item4">Link3</div>
-              <div className="navlink__Item5">Logout</div>
-            </>
-          ) : (
-            // IS THE USER CURRENTLY LOGGED OUT? 
-            <>
-              <div className="navlink__Item1">Home</div>
-              <div className="navlink__Item2">Link1</div>
-              <div className="navlink__Item3">Link2</div>
-              <div className="navlink__Item4">Link3</div>
-              <div className="navlink__Item5">Login</div>
-            </>
-          )}
-        </div>
-       )}
+         </>
+      )}
+
+      {/* THIS IS FOR DESKTOP DISPLAY  */}
+      {!isMobile && (
+         <>
+            <div className="logo__Menu__Container">
+              <h1>Cut Critic</h1>
+
+              {/* Add onClick event listener to MenuIcon to toggle the menu open and closed */}
+
+              <IconButton className='mobileIcons' alignItems="center" size='large'>
+                {showMenu ? (
+                  <CloseIcon fontSize='2em' onClick={toggleMenu} />
+                ) : (
+                  <MenuIcon fontSize='2em' onClick={toggleMenu} />
+                )}
+              </IconButton>
+            
+            
+
+            {/* Use conditional rendering to show the menu only when menuOpen is true */}
+          
+            {showMenu && (
+              <div className="navlinks">
+                {/* IS THE USER CURRENTLY LOGGED IN?  */}
+                {isLoggedIn ? (
+                  <>
+                    <div className="navlink__Item1">Home</div>
+                    <div className="navlink__Item2">Link1</div>
+                    <div className="navlink__Item3">Link2</div>
+                    <div className="navlink__Item4">Link3</div>
+                    <div className="navlink__Item5">Logout</div>
+                  </>
+                ) : (
+                  // IS THE USER CURRENTLY LOGGED OUT? 
+                  <>
+                    <div className="navlink__Item1">Home</div>
+                    <div className="navlink__Item2">Link1</div>
+                    <div className="navlink__Item3">Link2</div>
+                    <div className="navlink__Item4">Link3</div>
+                    <div className="navlink__Item5">Login</div>
+                  </>
+                )}
+              </div>
+            )}
+          </div>
+         </>
+      )}
 
         <input type="text" placeholder='Find services' />
       </div>
