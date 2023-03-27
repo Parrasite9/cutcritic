28371906cc403ae02e14db7f689d051ecf38fd43
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-03-26 19:11:04 -0500

    Navbar now has a ternary that displays menu. A Close Icon has been imported but not applied to the menu at this time.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './../CSS/Navbar.css'
 
 // MUI ICONS 
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Navbar() {
+
+  // Use state hook to track whether the menu is open or closed
+  const [showMenu, setShowMenu] = useState(false) 
+
+  // Create a function to toggle the menu open and closed
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <div className='navbar'>
       <div className="navbar__Container">
@@ -15,9 +25,25 @@ function Navbar() {
           <h1>Cut Critic</h1>
 
           <IconButton alignItems="center" size='large'>
-            <MenuIcon fontSize='2em' />
+
+            {/* Add onClick event listener to MenuIcon to toggle the menu open and closed */}
+            <MenuIcon fontSize='2em' onClick={toggleMenu} />
           </IconButton>
         </div>
+
+       {/* Use conditional rendering to show the menu only when menuOpen is true */}
+        {showMenu ? (
+          <>
+            <div className="navlink__Item1">Home</div>
+            <div className="navlink__Item2">Link1</div>
+            <div className="navlink__Item3">Link2</div>
+            <div className="navlink__Item4">Link3</div>
+            <div className="navlink__Item5">Link4</div>
+          </>
+        ) : (
+          <>
+          </>
+        )}
 
         <input type="text" placeholder='Find services' />
       </div>
