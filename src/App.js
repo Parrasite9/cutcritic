970ef18911fccba136824a4254b798f37370b1f6
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-03-30 12:29:01 -0500

    Signup component has been placed on the App.js for testing purposes, this needs to be removed when testing is complete. Signup.js has a form created and will be used for account creation. Firebase and Firestore JS has been created and updated with code from Firebase to obtain and hold user data.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 
 import './App.css'
+import SignUp from './Components/SignUp'
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <SignUp />
     </div>
   )
 }
--- src/Firebase/Firestore.js
+++ src/Firebase/Firestore.js
@@ -1,14 +1,30 @@
-import { collection, addDoc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkIw6H5rzBkKyiEhEKGZEPpfSbHgSB-5Q",
+    authDomain: "cutcritic-940d1.firebaseapp.com",
+    projectId: "cutcritic-940d1",
+    storageBucket: "cutcritic-940d1.appspot.com",
+    messagingSenderId: "313088351936",
+    appId: "1:313088351936:web:2885e5c086bf2e1d3cd725",
+    measurementId: "G-2XS9KRCCGX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export async function addUser(firstName, lastName, birthYear) {
-    try {
-        const docRef = await addDoc(collection(db, "users"), {
-          first: firstName,
-          last: lastName,
-          born: birthYear,
-        });
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: firstName,
+      last: lastName,
+      born: birthYear,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
+
+export { db };
