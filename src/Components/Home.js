3487e557ca748bf35f098f79fde121fa70e57a13
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-04-20 11:52:16 -0500

    Hero component added to home and Featured component created.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './../CSS/Hero.css'
 import Navbar from './Navbar'
+import Hero from './Hero'
+
 
 function Home({userId}) {
   return (
     <div className='home'>
       <Navbar userId={userId} />
+      <Hero />
+
+      
     </div>
   )
 }
