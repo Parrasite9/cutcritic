dfadcb33ce26c374b16a022fa24f6b777a1aea3e
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-04-25 11:32:36 -0500

    Hero section created, only mobime built.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,7 +4,16 @@ import './../CSS/Hero.css'
 function Hero() {
   return (
     <div className='hero'>
-      
+      <div className="hero__text">
+        <h1>Find Your Perfect Barber or Stylist</h1>
+        <p>Book appointments with independent barbers and stylists in your area. Our platform connects you with the best professionals for your unique needs.</p>
+      </div>
+
+      <div className="btn__container">
+        <button>View all Services</button>
+      </div>
+
+      <img src="/images/heroImg.png" alt="" />
     </div>
   )
 }
