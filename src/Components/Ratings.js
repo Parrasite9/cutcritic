414bfaded4967cfd09b671816902482dd1e6820f
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-04-27 14:13:46 -0500

    nothing changed in featured, Ratings JS has a few updates and CSS has more media queries.

--- src/Components/Ratings.js
+++ src/Components/Ratings.js
@@ -10,24 +10,25 @@ function Ratings() {
   return (
     <div className='ratings'>
       <div className="ratings__container">
+        {/* RATINGS ARE HARD CODED, THIS NEEDS TO BECOME DYNAMIC AT SOME POINT AND POSSIBLY PULLED FROM GOOGLE ITSELF.  */}
         <div className="ratings__grid1">
-            <h1>{value}</h1>
-            <Rating name='half-rating-read' value={value} precision={0.1} readOnly />
+            <h1><strong>{value}</strong></h1>
+            <Rating name='half-rating-read' value={value} precision={0.1} size="small" readOnly />
             <h5>2,384 Ratings</h5>
             <h5>Google Reviews</h5>
         </div>
 
 
         <div className="ratings__grid2">
-            <h1>A+</h1>
-            <Rating name='half-rating-read' value={value} precision={0.1} readOnly />
+            <h1><strong>A+</strong></h1>
+            <Rating name='half-rating-read' value={value} precision={0.1} size="small" readOnly />
             <h5>125 Reviews</h5>
             <h5>BBB Rating</h5>
         </div>
 
         <div className="trusted__by">
-            <h1>Trusted by numerous Grooming enthusiasts</h1>
-            <h2>Jessica Simon</h2>
+            <h2>Trusted by numerous Grooming enthusiasts</h2>
+            <h3>Jessica Simon</h3>
             <p>I love how easy it is to find and book appointments with the best barbers and stylist through this platform!</p>
         </div>
       </div>
